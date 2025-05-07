27eaee653b475739bc11c3293ab8cd3b47ae92fe
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2025-05-07 23:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const btn=document.getElementById("btn");
+btn.addEventListener("click",fun);
+
+function fun(e){
+	e.preventDefault();
+	let a=document.getElementById("age").value.trim();
+	let b=document.getElementById("name").value.trim();
+	if(a="" || b="")
+	{
+		alert("Please enter valid details");
+		return;
+	}
+	const  eligibility=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(a>=18)
+			{
+				resolve("Welcome, "+b+". You can vote.")
+			}
+			else{
+				reject(`oh sorry ${b}. You aren't old enough`);
+			}
+		},4000);
+		
+	});
+	elegibilty.then((message)=>{
+		alert(message);
+	})
+	.catch((eroor)=>{
+		alert(error);
+	});
+	
+}
